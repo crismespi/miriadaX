c063ebf856cab240fd883b5cd55de02426490779
Cristhian Manuel Espinoza Rodriguez <cristhian.fiis@gmail.com>
2014-09-19 22:40:23 -0500

    involuciona implementado

--- tactil.js
+++ tactil.js
@@ -11,6 +11,16 @@ $( function (){
 		
 	}
 
+	function involuciona(){
+		if(i.attr == "charmeleon.png"){
+			i.attr('src', 'charmander.png');
+		}
+		else{
+			i.attr('src', 'charmeleon.png');
+		}
+		
+	}
+
 	i.on('swipeRight', evoluciona);
 	i.on('swipeLeft', involuciona);
 });

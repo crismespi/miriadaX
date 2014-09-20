473a6eecab4d0047b33a702b24f71165d5763259
Cristhian Manuel Espinoza Rodriguez <cristhian.fiis@gmail.com>
2014-09-19 22:31:43 -0500

    Primeras imagenes y first commit

---
+++ tactil.js
@@ -0,0 +1,19 @@
+$( function (){
+	var i=$(#imagen);
+
+	function evoluciona(){
+		if(i.attr == "charmander.png"){
+			i.attr('src', 'charmeleon.png');
+		}
+		else{
+			i.attr('src', 'charizard.jpeg');
+		}
+		
+	}
+
+	i.on('swipeRight', evoluciona);
+	i.on('swipeLeft', involuciona)
+
+
+
+});
